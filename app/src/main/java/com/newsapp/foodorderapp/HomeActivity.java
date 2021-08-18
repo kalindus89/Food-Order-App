bae4dc1d682595f0504ccbfb537f7279c2bcd433
kalin <kalindusupun@gmail.com>
2021-08-18 16:01:14 +0530

    Food Order App Upload

--- app/src/main/java/com/newsapp/foodorderapp/HomeActivity.java
+++ app/src/main/java/com/newsapp/foodorderapp/HomeActivity.java
@@ -1,11 +1,26 @@
 package com.newsapp.foodorderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.newsapp.foodorderapp.singin_signup.WelcomeActivity;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    ImageView drawerIcon;
+
+    LinearLayout ll_First,ll_Second,ll_Third,ll_Fourth,ll_Fifth,ll_Sixth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +29,92 @@ public class HomeActivity extends AppCompatActivity {
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        onSetNavigationDrawerEvents();
+    }
+    private void onSetNavigationDrawerEvents() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+
+        //drawerLayout.openDrawer(GravityCompat.END);
+
+        drawerIcon = (ImageView) findViewById(R.id.drawerIcon);
+
+        ll_First = (LinearLayout) findViewById(R.id.ll_First);
+        ll_Second = (LinearLayout) findViewById(R.id.ll_Second);
+        ll_Third = (LinearLayout) findViewById(R.id.ll_Third);
+        ll_Fourth = (LinearLayout) findViewById(R.id.ll_Fourth);
+        ll_Fifth = (LinearLayout) findViewById(R.id.ll_Fifth);
+        ll_Sixth = (LinearLayout) findViewById(R.id.ll_Sixth);
+
+        drawerIcon.setOnClickListener(this);
+        ll_First.setOnClickListener(this);
+        ll_Second.setOnClickListener(this);
+        ll_Third.setOnClickListener(this);
+        ll_Fourth.setOnClickListener(this);
+        ll_Fifth.setOnClickListener(this);
+        ll_Sixth.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.drawerIcon:
+                drawerLayout.openDrawer(navigationView, true);
+                break;
+            case R.id.ll_First:
+                showToast("ll_First");
+                drawerLayout.closeDrawer(navigationView, true);
+                break;
+            case R.id.ll_Second:
+                showToast("ll_Second");
+                drawerLayout.closeDrawer(navigationView, true);
+                break;
+            case R.id.ll_Third:
+                showToast("ll_Third");
+                drawerLayout.closeDrawer(navigationView, true);
+                break;
+            case R.id.ll_Fourth:
+                showToast("ll_Fourth");
+                drawerLayout.closeDrawer(navigationView, true);
+                break;
+            case R.id.ll_Fifth:
+                showToast("ll_Fifth");
+                drawerLayout.closeDrawer(navigationView, true);
+                break;
+            case R.id.ll_Sixth:
+                showToast("tv_logout");
+                drawerLayout.closeDrawer(navigationView, true);
+
+                Intent intent = new Intent(HomeActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            default:
+                showToast("Default");
+                drawerLayout.closeDrawer(navigationView, true);
+                break;
+
+        }
+
+
+    }
+
+    private void showToast(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(navigationView)) {
+            drawerLayout.closeDrawer(navigationView, true);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
